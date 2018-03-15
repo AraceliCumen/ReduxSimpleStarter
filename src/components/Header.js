87e0eae26db4404b6bdb7c9a4183e64bd7495e54
AraceliCumen <aycuevam@gmail.com>
2018-03-14 23:40:59 -0500

    corrigiendo error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Header = ({person}) => (
+const Header = () => (
   <header>
      <nav className="navbar navbar-default">
         <div className="container-fluid">
           {/* Brand and toggle get grouped for better mobile display */}
           <div className="navbar-header">
-            <a className="navbar-brand" href="#">Header {person.name}</a>
+            <a className="navbar-brand" href="#">Header Araceli</a>
           </div>
         </div>{/* /.container-fluid */}
       </nav>
